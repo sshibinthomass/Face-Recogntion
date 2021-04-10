d7f29d25e33f0beef66471beb085c2e00fd2cf07
sshibinthomass <sshibinthomass@gmail.com>
2021-04-10 21:39:06 +0530

    start

--- facerec.py
+++ facerec.py
@@ -2,7 +2,7 @@ import face_recognition
 from PIL import Image
 import cv2
 
-image = face_recognition.load_image_file("my_pics/photo2.jpg")
+image = face_recognition.load_image_file("photos/pic1.jpg")
 face_locations = face_recognition.face_locations(image)
 
 for face_location in face_locations:
